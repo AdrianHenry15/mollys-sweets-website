dc5559c19ff9a92222069bd0883590e0f54c4807
Adrian <adrianhenry2115@gmail.com>
2022-12-30 13:02:24 -0500

    homepage layout plus media queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import "./App.scss"
-import Navbar from "./components/Navbar"
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import PageNotFound from "./pages/PageNotFound"
-import Cupcakes from "./pages/Cupcakes"
-import Cookies from "./pages/Cookies"
-import Cakes from "./pages/Cakes"
-import Footer from "./components/Footer"
+import "./App.scss";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+import Cupcakes from "./pages/Cupcakes";
+import Cookies from "./pages/Cookies";
+import Cakes from "./pages/Cakes";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="app snow">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/cakes" element={<Cakes />}></Route>
-        <Route path="/cookies" element={<Cookies />}></Route>
-        <Route path="/cupcakes" element={<Cupcakes />}></Route>
-        <Route path="/*" element={<PageNotFound />}></Route>
-      </Routes>
-      <Footer />
-    </div>
-  )
+    return (
+        <div className="app snow">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/cakes" element={<Cakes />}></Route>
+                <Route path="/cookies" element={<Cookies />}></Route>
+                <Route path="/cupcakes" element={<Cupcakes />}></Route>
+                <Route path="/*" element={<PageNotFound />}></Route>
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,32 @@
-import "../styles/Footer.scss"
+import "../styles/Footer.scss";
 
 const Footer = () => {
-  return (
-    <div className="footer-container">
-      <div className="contact-container">
-        <h2 className="contact-label">Contact Us</h2>
-        <a href="tel: (407) 242-4468" className="number">
-          (407) 242-4468
-        </a>
-        <a href="mailto: mollyspecialtysweets@gmail.com" className="email">
-          mollyspecialtysweets@gmail.com
-        </a>
-      </div>
-      <div className="location-container">
-        <h2 className="location-label">Location</h2>
-        <div className="locations">
-          2211 Allen Lane <br /> Winter Park, Florida 32792
-        </div>
-      </div>
-      <footer className="footer">
-        2022 Molly's Specialty Sweets, Made With Love. Website Created By Adrian
-        Henry
-      </footer>
-    </div>
-  )
-}
+    return (
+        <footer className="footer-container">
+            <div className="contact-container">
+                <h2 className="contact-label">Contact Us</h2>
+                <a href="tel: (407) 242-4468" className="number">
+                    (407) 242-4468
+                </a>
+                <a
+                    href="mailto: mollyspecialtysweets@gmail.com"
+                    className="email"
+                >
+                    mollyspecialtysweets@gmail.com
+                </a>
+            </div>
+            <div className="location-container">
+                <h2 className="location-label">Location</h2>
+                <div className="locations">
+                    2211 Allen Lane <br /> Winter Park, Florida 32792
+                </div>
+            </div>
+            <footer className="footer">
+                2022 Molly's Specialty Sweets, Made With Love. Website Created
+                By Adrian Henry
+            </footer>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
